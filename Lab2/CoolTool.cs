e638378453a4e0bdd22a7dbfc77ff3f56c1c6296
ASharp <a.sharp.fus@gmail.com>
2021-02-09 21:25:45 +0200

    cool tool yeah

--- Lab2/CoolTool.cs
+++ Lab2/CoolTool.cs
@@ -1,7 +1,24 @@
 ﻿namespace Lab2
 {
-    public class CoolTool
-    {
-        
-    }
+    public static int[] Sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length-i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int el = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = el;
+                    }
+                }
+            }
+
+            foreach (var elem in arr)
+            {
+                Console.Write($"{elem}\t");
+            }
+            return arr;
+        }
 }
