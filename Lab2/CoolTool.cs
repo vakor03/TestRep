9546ac3b57f090870e6df51ac55a67d8b0922161
Valerii_kor <myksv200@gmail.com>
2021-02-09 22:33:11 +0300

    Redo type of Matrix

--- Lab2/CoolTool.cs
+++ Lab2/CoolTool.cs
@@ -2,8 +2,47 @@
 
 namespace Lab2
 {
-    public static int[] Sort(int[] arr)
+   
+    public class CoolTool
+    {
+        private int lengthOfMatrix;
+
+        private int[][] matrix
         {
+            get
+            {
+                int[][] newMatr = new int[lengthOfMatrix][];
+                for (int i = 0; i < lengthOfMatrix; i++)
+                {
+                    newMatr[i] = new int[lengthOfMatrix];
+                }
+
+                Random rand = new Random();
+                for (int i = 0; i < lengthOfMatrix; i++)
+                {
+                    for (int j = 0; j < lengthOfMatrix; j++)
+                    {
+                        newMatr[i][j] = rand.Next(1, 10);
+                    }
+                }
+                return newMatr;
+            }
+        }
+
+        public void printMatrix()
+        {
+            for (int i = 0; i < lengthOfMatrix; i++)
+            {
+                for (int j = 0; j < lengthOfMatrix; j++)
+                {
+                    Console.WriteLine($"{this.matrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public int[] Sort()
+        {
+            int[] arr = matrix[0];
             for (int i = 1; i < arr.Length; i++)
             {
                 for (int j = 0; j < arr.Length-i; j++)
@@ -22,38 +61,6 @@ namespace Lab2
                 Console.Write($"{elem}\t");
             }
             return arr;
-        }
-    public class CoolTool
-    {
-        private int lengthOfMatrix;
-
-        private int[,] matrix
-        {
-            get
-            {
-                int[,] newMatr = new int[lengthOfMatrix,lengthOfMatrix];
-                Random rand = new Random();
-                for (int i = 0; i < lengthOfMatrix; i++)
-                {
-                    for (int j = 0; j < lengthOfMatrix; j++)
-                    {
-                        newMatr[i, j] = rand.Next(1, 10);
-                    }
-                }
-                return newMatr;
-            }
-        }
-
-        public void printMatrix()
-        {
-            for (int i = 0; i < lengthOfMatrix; i++)
-            {
-                for (int j = 0; j < lengthOfMatrix; j++)
-                {
-                    Console.WriteLine($"{this.matrix[i, j]} ");
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
