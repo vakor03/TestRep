407ec36851619bf11c30fff2a3a55ce4eba55ec8
Valerii_kor <myksv200@gmail.com>
2021-02-09 22:06:53 +0300

    Why are you running

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -7,6 +7,7 @@ namespace Lab2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Why are you running!");
         }
     }
 }
