3565f710f3158dab865b2d1cc1a19ddbb9087862
ASharp <a.sharp.fus@gmail.com>
2021-02-13 19:34:20 +0200

    reset

---
+++ LabTwo/LabTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
